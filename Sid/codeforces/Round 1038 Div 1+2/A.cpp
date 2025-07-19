1144f829ebd69cb652dd2c902ef9962d4232c9dc
Siddharth-Agg-arwal <aggarwal.siddharth2003@outlook.com>
2025-07-19 22:31:52 +0530

    cf contest

--- Sid/codeforces/Round 1038 Div 1+2/A.cpp
+++ Sid/codeforces/Round 1038 Div 1+2/A.cpp
@@ -12,6 +12,19 @@ int main()
     cin >> tc;
     while (tc--)
     {
+        int n, m;
+        cin >> n >> m;
+
+        if (n == 1 || m == 1)
+        {
+            cout << "NO\n";
+        }
+        else if (n <= 2 && m <= 2)
+            cout << "NO\n";
+        else
+        {
+            cout << "YES\n";
+        }
     }
 
     return 0;
